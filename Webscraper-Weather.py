099dd381e75278148072e8204c333c0a7c8afa5b
Praveen K Nair <praveenknair2000@gmail.com>
2021-01-03 23:04:56 +0530

    Add files via upload

---
+++ Webscraper-Weather.py
@@ -0,0 +1,30 @@
+#importing Libraries
+import requests
+from bs4 import BeautifulSoup as soup
+import pandas as pd
+
+#url
+url ='https://mausam.imd.gov.in/'
+
+#requesting and parsing html
+page = requests.get(url)
+soup = soup(page.content,'html.parser')
+
+#saving all data into container
+container = soup.find_all(class_ = 'capital')
+
+#saving each values in separate arrays
+Places = [item.h3.text for item in container]
+Temp = [item.find(class_ = 'now').get_text() for item in container]
+Wind = [item.find(class_ = 'wind').get_text() for item in container]
+
+#converting to dataframe
+Weather_data = pd.DataFrame({
+                            'Places':Places,
+                            'Temperature':Temp,
+                            'Wind':Wind
+                            })
+
+#coverting to csv
+Weather_data.to_csv('Weather.csv')
+
